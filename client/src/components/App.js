30355c677b170856ae54a9d356e6bf06453fe6f1
cdnicoll <cdnicoll@gmail.com>
2018-11-27 09:11:28 -0800

    Added react client

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+        <div><p>Hello World - from client</p></div>
+    );
+  }
+}
+
+export default App;
