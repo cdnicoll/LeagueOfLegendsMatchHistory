9dcdd838b01c7504a1b61960134c4af599dc9723
cdnicoll <cdnicoll@gmail.com>
2018-11-27 09:16:29 -0800

    Added logging and cor headers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const LeagueJs = require("leaguejs");
 
 const app = express();
+
+app
+  .use(cors())
+  .use(morgan('combined'));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
