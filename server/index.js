ef74bfb785b60d4a8a42c4996dd5a5971b27c7a1
cdnicoll <cdnicoll@gmail.com>
2018-11-27 09:35:47 -0800

    Added getSummoner API route

--- server/index.js
+++ server/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const LeagueJs = require("leaguejs");
-const { LOL_API_KEY, PLATFORM_ID } = require("./config.json");
+const getSummoner = require('./routes/getSummoner');
 
-const leagueJs = new LeagueJs(LOL_API_KEY, { PLATFORM_ID });
 const app = express();
 
 app
   .use(cors())
-  .use(morgan('combined'));
+  .use(morgan('combined'))
+  .use('/getSummoner', getSummoner);
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-app.listen(3001);
+  app.listen(3001, () => {
+    console.log('App listening on port 3001, Lets go!');
+  });
---
+++ server/routes/getSummoner.js
@@ -0,0 +1,17 @@
+const routes = require('express').Router();
+const LeagueJs = require("leaguejs");
+const { LOL_API_KEY, PLATFORM_ID } = require("../config.json");
+
+
+const leagueJs = new LeagueJs(LOL_API_KEY, { PLATFORM_ID });
+
+
+routes.get('/:name', async (req, res) => {
+    try {
+        const summoner = await leagueJs.Summoner.gettingByName(req.params.name);
+        res.status(200).json(summoner);
+      } catch (err) {
+        res.status(400).send(err.response.body);
+      }});
+
+module.exports = routes;
