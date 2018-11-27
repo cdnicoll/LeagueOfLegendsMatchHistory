fe73d9f1a52b6169bb936452be3cbdeba3d39045
cdnicoll <cdnicoll@gmail.com>
2018-11-27 09:18:41 -0800

    Setup LeagueJS config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const LeagueJs = require("leaguejs");
+const { LOL_API_KEY, PLATFORM_ID } = require("./config.json");
 
+const leagueJs = new LeagueJs(LOL_API_KEY, { PLATFORM_ID });
 const app = express();
 
 app
