78dc33bceec2f4b378e381d90960b97f6732c89f
Cody Nicoll <cdnicoll@gmail.com>
2018-11-27 18:19:10 -0800

    Added Name and Creepscore to Match Card

--- client/src/components/MatchCard.js
+++ client/src/components/MatchCard.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Image, Segment, Header } from 'semantic-ui-react';
 
 const MatchCard = (props) => {
-  const { championName, summonerName, championLevel, kda } = props.match;
+  const {
+    championName,
+    summonerName,
+    championLevel,
+    kda,
+    totalCreepScore,
+    creepScorePerMinutes,
+  } = props.match;
   return (
     <Segment className="ui message five column grid floating">
       <div className="column">
@@ -15,6 +22,20 @@ const MatchCard = (props) => {
           K/D/A:
           {kda}
         </Header>
+      </div>
+      <div className="column">
+        <div className="row">
+          <Header as="h2">{championName}</Header>
+          <Header as="h5">{summonerName}</Header>
+        </div>
+        <div className="row">
+          <Header as="h5">Creep Score/Time</Header>
+          <p>
+            {totalCreepScore}
+            /
+            {creepScorePerMinutes}
+          </p>
+        </div>
       </div>
     </Segment>
   );
